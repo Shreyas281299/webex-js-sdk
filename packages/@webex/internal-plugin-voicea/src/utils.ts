33f90e52ac21c00d0a6b9d430fa4ac7729d6da05
Shreyas Sharma <shreysh2@cisco.com>
2024-07-25 15:46:47 +0530

    fix(voicea): testing

--- packages/@webex/internal-plugin-voicea/src/utils.ts
+++ packages/@webex/internal-plugin-voicea/src/utils.ts
@@ -2,6 +2,7 @@
 export const millisToMinutesAndSeconds = (millis: number) => {
   const minutes = Math.floor(millis / 60000);
   const seconds = ((millis % 60000) / 1000).toFixed(0);
+  //  some test comment
 
   return `${minutes}:${+seconds < 10 ? '0' : ''}${seconds}`;
 };
